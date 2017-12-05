18894b82975396d4d6b6eb6d1131090133e48790
Alexis Licea <alexisrogelioleon@gmail.com>
2017-12-05 15:04:06 -0600

    Se han agregado nuevas características como un HUD en el cual se muestran las monedas recolectadas, ademas de haber implementado el numero de corazones que se tienen. También se ha agregado un fondo para que sea dinámico desacuerdo a la poción del pingüino.

--- Ejemplo 1/Coin.swift
+++ Ejemplo 1/Coin.swift
@@ -11,35 +11,40 @@ import SpriteKit
 
 class Coin: SKSpriteNode, GameSprite {
     var initialSize = CGSize(width: 26, height: 26)
-    var textureAtlas:SKTextureAtlas = SKTextureAtlas(named: "Environment")
+    var textureAtlas:SKTextureAtlas =
+        SKTextureAtlas(named: "Environment")
     var value = 1
     
     init() {
         let bronzeTexture = textureAtlas.textureNamed("coin-bronze")
-        super.init(texture: bronzeTexture, color: .clear, size: initialSize)
-        self.physicsBody = SKPhysicsBody(circleOfRadius: size.width/2)
+        super.init(texture: bronzeTexture, color: UIColor.clear,
+                   size: initialSize)
+        self.physicsBody = SKPhysicsBody(circleOfRadius:
+            size.width / 2)
         self.physicsBody?.affectedByGravity = false
+        
         self.physicsBody?.categoryBitMask = PhysicsCategory.coin.rawValue
         self.physicsBody?.collisionBitMask = 0
     }
     
     // A function to transform this coin into gold!
     func turnToGold() {
-        self.texture = textureAtlas.textureNamed("coin-gold")
+        self.texture =
+            textureAtlas.textureNamed("coin-gold")
         self.value = 5
-    }
-    
-    func onTap() {}
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
     }
     
     func collect() {
         // Prevent further contact:
         self.physicsBody?.categoryBitMask = 0
         // Fade out, move up, and scale up the coin:
-        let collectAnimation = SKAction.group([SKAction.fadeAlpha(to: 0, duration: 0.2),SKAction.scale(to: 1.5, duration: 0.2),
-            SKAction.move(by: CGVector(dx: 0, dy: 25),duration: 0.2)])
+        let collectAnimation = SKAction.group([
+            SKAction.fadeAlpha(to: 0, duration: 0.2),
+            SKAction.scale(to: 1.5, duration: 0.2),
+            SKAction.move(by: CGVector(dx: 0, dy: 25),
+                          duration: 0.2)
+            ])
+        // After fading it out, move the coin out of the way
         // and reset it to initial values until the encounter
         // system re-uses it:
         let resetAfterCollected = SKAction.run {
@@ -47,11 +52,20 @@ class Coin: SKSpriteNode, GameSprite {
             self.alpha = 1
             self.xScale = 1
             self.yScale = 1
-            self.physicsBody?.categoryBitMask = PhysicsCategory.coin.rawValue
+            self.physicsBody?.categoryBitMask =
+                PhysicsCategory.coin.rawValue
         }
         // Combine the actions into a sequence:
-        let collectSequence = SKAction.sequence([collectAnimation, resetAfterCollected])
+        let collectSequence = SKAction.sequence([
+            collectAnimation,
+            resetAfterCollected
+            ])
         // Run the collect animation:
         self.run(collectSequence)
+    }
+    
+    func onTap() {}
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 }
--- Ejemplo 1/GameScene.swift
+++ Ejemplo 1/GameScene.swift
@@ -9,70 +9,184 @@
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    
     let cam = SKCameraNode()
-    let player = Player()
+    let hud = HUD()
     let ground = Ground()
-    var screenCenterY = CGFloat()
-    let initialPlayersPosition = CGPoint(x: 150,y: 250)
-    var playerProgress = CGFloat()
+    let player = Player()
     let encounterManager = EncounterManager()
-    var nextEncounterSpawnPosition = CGFloat(150)
     let powerUpStar = Star()
+    var backgrounds:[Background] = []
+    
+    var screenCenterY = CGFloat()
+    
+    let initialPlayerPosition = CGPoint(x: 150, y: 250)
+    var playerProgress = CGFloat()
+    
+    var nextEncounterSpawnPosition = CGFloat(150)
+    
     var coinsCollected = 0
     
     override func didMove(to view: SKView) {
         self.anchorPoint = .zero
-        self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
+        self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue:
+            0.95, alpha: 1.0)
+        
+        // Assign the camera to the scene
         self.camera = cam
+        
+        // Add the ground to the scene:
         ground.position = CGPoint(x: -self.size.width * 2, y: 30)
         ground.size = CGSize(width: self.size.width * 6, height: 0)
         ground.createChildren()
         self.addChild(ground)
-        player.position = initialPlayersPosition
+        
+        // Add the player to the scene:
+        player.position = initialPlayerPosition
         self.addChild(player)
+        
+        // Set gravity
         self.physicsWorld.gravity = CGVector(dx: 0, dy: -5)
-        screenCenterY = self.size.height/2
-        encounterManager.encounters[0].position = CGPoint(x: 400, y: 330)
+        
+        // Store the vertical center of the screen:
+        screenCenterY = self.size.height / 2
+        
         encounterManager.addEncountersToScene(gameScene: self)
+        
+        // Place the star out of the way for now
         self.addChild(powerUpStar)
         powerUpStar.position = CGPoint(x: -2000, y: -2000)
+        
         self.physicsWorld.contactDelegate = self
+        
+        // Add the camera itself to the scene's node tree:
+        self.addChild(self.camera!)
+        // Position the camera node above the game elements:
+        self.camera!.zPosition = 50
+        // Create the HUD's child nodes:
+        hud.createHudNodes(screenSize: self.size)
+        // Add the HUD to the camera's node tree:
+        self.camera!.addChild(hud)
+        
+        // Instantiate three Backgrounds to the backgrounds array:
+        for _ in 0..<3 {
+            backgrounds.append(Background())
+        }
+        // Spawn the new backgrounds:
+        backgrounds[0].spawn(parentNode: self,
+                             imageName: "background-front", zPosition: -5,
+                             movementMultiplier: 0.75)
+        backgrounds[1].spawn(parentNode: self,
+                             imageName: "background-middle", zPosition: -10,
+                             movementMultiplier: 0.5)
+        backgrounds[2].spawn(parentNode: self,
+                             imageName: "background-back", zPosition: -15,
+                             movementMultiplier: 0.2)
     }
     
     override func didSimulatePhysics() {
-        playerProgress = player.position.x - initialPlayersPosition.x
+        // Keep track of how far the player has flown
+        playerProgress = player.position.x -
+            initialPlayerPosition.x
+        
+        // Keep the camera locked at mid screen by default:
         var cameraYPos = screenCenterY
         cam.yScale = 1
         cam.xScale = 1
+        
+        // Follow the player up if they're higher than half the screen:
         if (player.position.y > screenCenterY) {
             cameraYPos = player.position.y
-            let percentOfMaxHeight = (player.position.y - screenCenterY) / (player.maxHeight - screenCenterY)
+            // Scale out the camera as they go higher:
+            let percentOfMaxHeight = (player.position.y -
+                screenCenterY) / (player.maxHeight -
+                    screenCenterY)
             let newScale = 1 + percentOfMaxHeight
             cam.yScale = newScale
             cam.xScale = newScale
         }
-        self.camera!.position = CGPoint(x: player.position.x, y:cameraYPos)
+        
+        // Move the camera for our adjustment:
+        self.camera!.position = CGPoint(x: player.position.x, y: cameraYPos)
+        
+        // Check to see if the ground should jump forward:
         ground.checForReposition(playerProgress: playerProgress)
+        
         // Check to see if we should set a new encounter:
         if player.position.x > nextEncounterSpawnPosition {
-            encounterManager.placeNextEncounter(currentXPos: nextEncounterSpawnPosition)
+            encounterManager.placeNextEncounter(
+                currentXPos: nextEncounterSpawnPosition)
             nextEncounterSpawnPosition += 1200
+            
             // Each encounter has a 10% chance to spawn a star:
             let starRoll = Int(arc4random_uniform(10))
             if starRoll == 0 {
                 // Only move the star if it is off the screen.
-                if abs(player.position.x - powerUpStar.position.x) > 1200 {
+                if abs(player.position.x - powerUpStar.position.x)
+                    > 1200 {
                     // Y Position 50-450:
-                    let randomYPos = 50 + CGFloat(arc4random_uniform(400))
-                    powerUpStar.position = CGPoint(x: nextEncounterSpawnPosition, y: randomYPos)
+                    let randomYPos = 50 +
+                        CGFloat(arc4random_uniform(400))
+                    powerUpStar.position = CGPoint(x:
+                        nextEncounterSpawnPosition, y: randomYPos)
                     // Remove any previous velocity and spin:
                     powerUpStar.physicsBody?.angularVelocity = 0
                     powerUpStar.physicsBody?.velocity =
-                    CGVector(dx: 0, dy: 0)
+                        CGVector(dx: 0, dy: 0)
                 }
-                
             }
+        }
+        
+        // Position the backgrounds:
+        for background in self.backgrounds {
+            background.updatePosition(playerProgress: playerProgress)
+        }
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        // Each contact has two bodies; we do not know which is which.
+        // We will find the penguin body, then use
+        // the other body to determine the type of contact.
+        let otherBody:SKPhysicsBody
+        // Combine the two penguin physics categories into one
+        // bitmask using the bitwise OR operator |
+        let penguinMask = PhysicsCategory.penguin.rawValue |
+            PhysicsCategory.damagedPenguin.rawValue
+        // Use the bitwise AND operator & to find the penguin.
+        // This returns a positive number if body A's category
+        // is the same as either the penguin or damagedPenguin:
+        if (contact.bodyA.categoryBitMask & penguinMask) > 0 {
+            // bodyA is the penguin, we will test bodyB:
+            otherBody = contact.bodyB
+        }
+        else {
+            // bodyB is the penguin, we will test bodyA:
+            otherBody = contact.bodyA
+        }
+        // Find the type of contact:
+        switch otherBody.categoryBitMask {
+        case PhysicsCategory.ground.rawValue:
+            print("hit the ground")
+            player.takeDamage()
+            hud.setHealthDisplay(newHealth: player.health)
+        case PhysicsCategory.enemy.rawValue:
+            print("take damage")
+            player.takeDamage()
+            hud.setHealthDisplay(newHealth: player.health)
+        case PhysicsCategory.coin.rawValue:
+            // Try to cast the otherBody's node as a Coin:
+            if let coin = otherBody.node as? Coin {
+                // Invoke the collect animation:
+                coin.collect()
+                // Add the value of the coin to our counter:
+                self.coinsCollected += coin.value
+                hud.setCoinCountDisplay(newCoinCount:
+                    self.coinsCollected)
+                print(self.coinsCollected)
+            }
+        case PhysicsCategory.powerup.rawValue:
+            player.starPower()
+        default:
+            print("Contact with no game logic")
         }
     }
     
@@ -88,41 +202,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 gameSprite.onTap()
             }
         }
+        
         player.startFlapping()
-    }
-    
-    func didBegin(_ contact: SKPhysicsContact) {
-        let otherBody:SKPhysicsBody
-        let penguinMask = PhysicsCategory.penguin.rawValue | PhysicsCategory.damagedPenguin.rawValue
-        if (contact.bodyA.categoryBitMask & penguinMask) > 0 {
-            otherBody = contact.bodyB
-        }
-        else {
-            otherBody = contact.bodyA
-        }
-        switch otherBody.categoryBitMask {
-            case PhysicsCategory.ground.rawValue:
-                print("hit the ground")
-                player.takeDamage()
-            case PhysicsCategory.enemy.rawValue:
-                print("take damage")
-                player.takeDamage()
-            case PhysicsCategory.coin.rawValue:
-                print("collect a coin")
-            case PhysicsCategory.powerup.rawValue:
-                print("start the power-up")
-            case PhysicsCategory.coin.rawValue:
-                // Try to cast the otherBody's node as a Coin:
-                if let coin = otherBody.node as? Coin {
-                    // Invoke the collect animation:
-                    coin.collect()
-                        // Add the value of the coin to our counter:
-                    self.coinsCollected += coin.value
-                    print(self.coinsCollected)
-            }
-            default:
-                print("Contact with no game logic")
-        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -132,6 +213,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         player.stopFlapping()
     }
+    
     
     override func update(_ currentTime: TimeInterval) {
         player.update()
